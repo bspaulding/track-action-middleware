d3a2e403269c4ab420682cedbda426ecaa634f8d
Bradley Spaulding <brad.spaulding@gmail.com>
2016-10-27 16:37:56 -0700

    prepublish enforces NODE_ENV, not using babel-preset-es2015-rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,11 @@
+/* @flow */
+
 import babel from 'rollup-plugin-babel';
 
 export default {
 	dest: 'bundle.js',
 	entry: 'src/index.js',
-	plugins: [babel({
-		babelrc: false,
-		presets: ["es2015-rollup", "stage-1"],
-		plugins: ["transform-flow-strip-types"]
-	})],
+	plugins: [babel()],
 	format: 'umd',
 	moduleName: 'trackActionMiddleware'
 };
