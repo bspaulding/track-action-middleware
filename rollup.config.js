3bc8a7ec5fe649c046f7ea0138181ec588a7fba4
Bradley Spaulding <brad.spaulding@gmail.com>
2016-10-26 22:26:50 -0700

    rollup config outputs to bundle.js, pointing package main to bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
+	dest: 'bundle.js',
 	entry: 'src/index.js',
-	plugins: [babel({
-		presets: ['es2015-rollup', 'stage-1'],
-		plugins: ['transform-flow-strip-types']
-	})],
+	plugins: [babel()],
 	format: 'umd',
 	moduleName: 'track-action-middleware'
 };
